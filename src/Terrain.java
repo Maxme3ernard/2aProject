4f2450812951fa2a2eb2fabd8f4f977ea7f47461
Maxme3ernard <maximebernardmax@gmail.com>
2018-02-15 15:25:06 +0100

    Ajout Classe Terrain

---
+++ src/Terrain.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Maxime on 15/02/2018.
+ */
+public class Terrain {
+    private int hauteur;
+    private int largeur;
+    private int[][] terrain;
+
+    public Terrain(int h, int l){
+        terrain=new int[h][l];
+    }
+}
