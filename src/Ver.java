676dc3aa199444c663fa95943f35376a90ef5200
Maxme3ernard <maximebernardmax@gmail.com>
2018-02-15 15:17:16 +0100

    Ajout Classes

--- src/Ver.java
+++ src/Ver.java
@@ -1,5 +1,24 @@
+import java.awt.*;
+
 /**
  * Created by Maxime on 15/02/2018.
  */
 public class Ver {
+    private Color couleur;
+    private String name;
+    private int life;
+    private int x;
+    private int y;
+
+
+    public Ver(int t, String n){ //t=0 ou 1, pour savoir quelle équipe
+        if(t==0) couleur=Color.blue;
+        else couleur=Color.red;
+        name=n;
+        life=200;
+        //générateur aléatoire position
+    }
+    public void modifierVie(int hp){
+        life+=hp;
+    }
 }
