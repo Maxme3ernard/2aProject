df7ae436e4b7b0d4096d6506541a5e85a5aa5b71
Reddragio <jacques.r.charnay@hotmail.fr>
2018-02-15 15:23:27 +0100

    Remplacement des private par protected

--- src/Ver.java
+++ src/Ver.java
@@ -4,11 +4,11 @@ import java.awt.*;
  * Created by Maxime on 15/02/2018.
  */
 public class Ver {
-    private Color couleur;
-    private String name;
-    private int life;
-    private int x;
-    private int y;
+    protected Color couleur;
+    protected String name;
+    protected int life;
+    protected int x;
+    protected int y;
 
 
     public Ver(int t, String n){ //t=0 ou 1, pour savoir quelle équipe
