98e8ab2a6a8c1879728e3a3ee21cac3aca3d39dc
Maxme3ernard <maximebernardmax@gmail.com>
2018-02-15 19:03:02 +0100

    Generation Terrain aléatoire

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -10,21 +10,43 @@ public class Fenetre extends JFrame {
 
     public Fenetre(int s /*int[][] t*/){
         super("Worm FighterZ");
-        int[][] t={{0,0,0,0,0,0,0,0,0,0}, //amené à disparaitre quand la génération du terrain sera automatique
+        genererTerrain(100,50);
+        /* int[][] t={{0,0,0,0,0,0,0,0,0,0}, //amené à disparaitre quand la génération du terrain sera automatique
                 {0,0,0,0,0,1,1,0,0,0},
                 {0,0,0,0,0,1,1,1,1,0},
                 {0,0,0,0,1,1,1,1,1,1},
                 {0,0,0,1,1,1,1,1,1,1},
                 {2,2,2,1,1,1,1,1,1,1},
                 {2,2,2,1,1,1,1,1,1,1}};
-        terrain=t;
+        terrain=t;*/
         blockSize=s;
-        setSize((t[0].length+1)*blockSize,(t.length+2)*blockSize);
+        setSize((terrain[0].length+1)*blockSize,(terrain.length+2)*blockSize);
         setLocation(10,10);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanelFenetre f=new JPanelFenetre(blockSize, terrain);
         setContentPane(f);
         setVisible(true);
+    }
+
+    public void genererTerrain(int x, int y){
+        int[][] t=new int[y][x];
+        int p=(int) y/2;
+        int r;
+        for(int i=0;i<t[0].length;i++){
+            r=(int)((Math.random()*4-Math.random()*4));
+            System.out.println(r);
+            if(p>y-10) p-=Math.abs(r);
+            if(p<10) p+=Math.abs(r);
+            else p+=r;
+            if(p<t.length && p>0){
+                System.out.println(p+"   "+i);
+                t[p][i]=1;
+                for(int j=p;j<t.length;j++){
+                    t[j][i]=1;
+                }
+            }
+        }
+        terrain=t;
     }
 }
